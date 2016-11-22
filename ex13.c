182c50b41db8420b0b57bb67f68511984a157bbc
kevinlmadison <coolklm121@gmail.com>
2016-11-22 17:41:43 -0500

    added ex14

--- ex13.c
+++ ex13.c
@@ -10,9 +10,18 @@ int main(int argc, char *argv[])
     }
 
     //Let's make our own array of strings
-    char *states[] = {"California", "Oregon", "Washington", "Texas"};
+    char *states[] = {"California", "Oregon", "Washington", "Texassssss"};
 
     int num_states = 4;
+    //for now this is not working to check the size of each part of the array
+    long state_size = sizeof(states);
+    long *state_sizes = { 0 };
+    for (int i = 0; i < 4; i++){
+        char *state = states[i];
+        state_sizes[i] = sizeof(state);
+        printf("State: %s, Size %ld \n", states[i], state_sizes[i]);
+    }
+    printf("State Size: %ld \n", state_size);
 
     for (i = 0; i < num_states; i++){
         printf("State %d: %s\n", i, states[i]);

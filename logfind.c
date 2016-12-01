94c4f90d673c766316cebe83367b896f4f5a10da
kevinlmadison <coolklm121@gmail.com>
2016-12-01 16:28:13 -0500

    still working on logfind

--- logfind.c
+++ logfind.c
@@ -1,16 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <glob.h>
 #include "dbg.h"
 
+const unsigned int MAX_LINES = 1024;
 /**
  * This program searches for logfiles that contain 
  * the word given to this program as arguments
  */
 
+/**
+ * So here is where I learn what glob does, basically
+ * its a structure that contains the numbers of finds
+ * and paths to those finds
+ */
 
-const unsigned int MAX_LINES = 1024;
+int list_files(glob_t *pglob){
+    char *lines = callac(MAX_LINES, 1);
+    FILE *file = fopen(".logfind", "r");
+    int glob_flags = GLOB_TILDE;
+    int i = 0;
+    int rc = -1;
 
+    check(pglob != NULL, "Incorrect glob_t given.");
+    check_mem(lines);
+    check(file, "Failed to open .logfind file.");
+
+    while(fgets(lines, MAX_LINES - 1, file) != NULL) {
+        lines[strlen(lines) - 1] = '\0';
+        debug("Globbing %s", lines);
+        rc = glob(lines, glob_flags, NULL, pglob);
+        check(rc == 0 || rc == GLOB_NOMATCH, "Failed to glob");
+        
+        //Interesting way to now add GLOB_APPEND since we can't
+        //have it as a flag upon instantiation
+        if (glob_flags == GLOB_TILDE) glob_flags |= GLOB_APPEND;
+    }
+
+    for (i = 0; i < pglob->gl_pathc; i++) {
+        debug("Matched file: %s\n", pglob->gl_pathv[i]);
+    }
+    rc = 0;
+
+    free(lines);
+    fclose(file);
+    return 0;
+
+error:
+    if(lines) free(lines);
+    if(file) fclose(file);
+    return rc;
+}
+
+//This function will parse our arguments and check for the -o flag
+int parse_args(int *use_or, int *argc, char **argv[])
+{
+    //We want to skip the first argument because thats "./logfind"
+    (*argc)--;
+    (*argv)++;
+
+    if(strcpr((*argv)[0], "-o") == 0) {
+        *use_or = 1;
+        (*argc)--;
+        (*argv)++;
+        check(*argc > 1, "You need words after -o");
+    } else {
+        *use_or = 0;
+    }
+    return 0;
+
+error:
+    return -1;
+
+}
 int scanner(const char *filename, int args_num, char *search_for[])
 {
 
@@ -53,13 +116,18 @@ error:
 
 int main(int argc, char *argv[])
 {
-    check(argc > 2, "usage: logfind word word word");
-    
+    int i = 0;
+    glob_t matched_files;
+    check(list_files(&matched_files) == 0, "Failed to list files.");
+    check(argc > 1, "usage: logfind word word word");
+    for (i = 0; i < matched_files.gl_pathc; i++) {
+        scanner(matched_files.gl_pathv[i], argc, argv);
+    }
     /*so this first function needs to return a list
      *of files that contain inclusively the arguments
      *given in the filename.
      */
-
+    
     scanner("logfind.c",argc, argv);
 
     return 0;
